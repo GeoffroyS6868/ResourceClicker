1db96e089b67a958ad693b10d35b60c0d752828c
Geoffroy <geoffroy-.stanger-@epitech.eu>
2024-03-13 16:18:10 +0100

    feat: Added a basic clicker

---
+++ Assets/Manager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public Text ClicksTotalText;
+
+    float TotalClicks;
+
+    public void AddClicks()
+    {
+        TotalClicks++;
+        ClicksTotalText.text = TotalClicks.ToString("0");
+    }
+}
